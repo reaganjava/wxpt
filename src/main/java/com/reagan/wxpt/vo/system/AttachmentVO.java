9abdd805006dad0e4ac6f42f0f4e19d713aa6c69
reaganjava <reaganjava@gmail.com>
2014-03-28 14:33:58 +0800

    w

---
+++ src/main/java/com/reagan/wxpt/vo/system/AttachmentVO.java
@@ -0,0 +1,7 @@
+package com.reagan.wxpt.vo.system;
+
+import com.reagan.wxpt.vo.BaseVO;
+
+public class AttachmentVO extends BaseVO {
+
+}
---
+++ src/main/java/com/reagan/wxpt/vo/system/CommandVO.java
@@ -0,0 +1,7 @@
+package com.reagan.wxpt.vo.system;
+
+import com.reagan.wxpt.vo.BaseVO;
+
+public class CommandVO extends BaseVO {
+
+}
